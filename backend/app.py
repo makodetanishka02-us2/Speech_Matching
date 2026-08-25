98532d2392caeaf41c3cd18946c1ffc163f07dad
makodetanishka02-us2 <makodetanishka02@gmail.com>
2026-08-25 23:10:19 +0530

    Port change for hosting

--- backend/app.py
+++ backend/app.py
@@ -146,7 +146,7 @@ def compare():
 
 if __name__ == "__main__":
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
